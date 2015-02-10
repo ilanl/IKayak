450237e319b03e8d9aa1416aeb1459d0ad914f46
ilanl <ilan.levy78@gmail.com>
2015-02-10 13:29:14 +0200

    adding watch target with main interface only

---
+++ KayApp/kayapp WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  kayapp WatchKit Extension
+//
+//  Created by Ilan Levy on 2/10/15.
+//  Copyright (c) 2015 Ilan Levy. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
