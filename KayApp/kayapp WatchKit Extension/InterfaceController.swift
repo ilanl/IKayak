417172f3377c33cb8fc4aa4623b69cfac7b80a12
ilanl <ilan.levy78@gmail.com>
2015-02-10 13:39:48 +0200

    using swift as watch kit target

---
+++ KayApp/kayapp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  kayapp WatchKit Extension
+//
+//  Created by Ilan Levy on 2/10/15.
+//  Copyright (c) 2015 Ilan Levy. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
